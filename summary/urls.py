58f20e0ad4e5046102a16a8f1701eaf648aa14d2
seolhyebom <seolhyebom@gmail.com>
2024-09-18 23:19:01 +0900

    AI_QUIZ 페이지 수정

--- summary/urls.py
+++ summary/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('summary_detail/<str:lecture_name>/<str:chapter_name>/', views.summary_detail_view, name='summary_detail'),  # summary_detail
     path('AI_summarypage/<str:lecture_name>/<str:chapter_name>/', views.AI_summarypage_view, name='AI_summarypage'),  # AI_summarypage
     path('AI_quizpage/<str:lecture_name>/<str:chapter_name>/', views.AI_quizpage_view, name='AI_quizpage'),  # AI_quizpage
+    path('AI_QUIZ/<str:lecture_name>/<str:chapter_name>/', views.AI_QUIZ_view, name='AI_QUIZ'),  # AI_QUIZ
+
 
     path('upload_file_summary/<str:lecture_name>/<str:chapter_name>/', views.upload_file_summary, name='upload_file_summary'),
     path('delete_file_summary/<int:file_id>/', views.delete_file_summary, name='delete_file_summary'),
--- summary/views.py
+++ summary/views.py
@@ -307,6 +307,99 @@ def AI_quizpage_view(request, lecture_name, chapter_name):
 
 
 
+def AI_QUIZ_view(request, lecture_name, chapter_name):
+    # 강의명과 챕터명이 일치하는 LectureChapter 객체를 가져옴
+    chapter = LectureChapter.objects.filter(lecture__title=lecture_name, chapter_name=chapter_name).first()
+
+    # 현재 로그인한 사용자 정보를 가져옴
+    current_user = request.user
+    lecture_chapters = LectureChapter.objects.filter(user=current_user).select_related('lecture').order_by('lecture__title')
+
+    # LectureChapter가 없는 경우 404 에러 반환
+    if not chapter:
+        raise Http404("챕터를 찾을 수 없습니다.")
+
+    lectures = []
+    for chapter_obj in lecture_chapters:
+        lecture_title = chapter_obj.lecture.title
+        chapter_name = chapter_obj.chapter_name
+        lecture_url = reverse('user:lecture_detail', kwargs={'lecture_name': lecture_title})
+        chapter_url = reverse('upload:chapter_detail', kwargs={'lecture_name': lecture_title, 'chapter_name': chapter_name})
+
+        # 현재 강의가 lectures 리스트에 없으면 추가
+        if not any(lecture['lecture'] == lecture_title for lecture in lectures):
+            lectures.append({'lecture': lecture_title, 'chapters': []})
+
+        # 현재 챕터 추가
+        lectures[-1]['chapters'].append({'chapter_name': chapter_name, 'chapter_url': chapter_url, 'lecture_url': lecture_url})
+
+    # 해당 챕터에 업로드된 파일들 가져오기
+    uploaded_files = UploadFile_summary.objects.filter(chapter=chapter)
+
+    # 파일 업로드를 위한 폼 생성
+    form = UploadFile_summaryForm(request.POST or None, request.FILES or None)    
+
+    # 현재 사용자의 친구 요청 가져오기
+    friend_requests = FriendRequest.objects.filter(to_user=request.user)
+
+    # 현재 사용자의 친구 목록 가져오기
+    user = request.user
+    friends = Friendship.objects.filter(user=user).select_related('friend')
+
+    # 오늘의 날짜 범위 계산
+    user_timezone = pytz.timezone('Asia/Seoul')  # 사용자의 시간대로 설정
+    today = timezone.now().astimezone(user_timezone).date()
+    start_of_day = timezone.make_aware(datetime.combine(today, datetime.min.time()))
+    end_of_day = start_of_day + timedelta(days=1)
+
+    # 현재 사용자의 모든 공부 세션 가져오기 (날짜 역순 정렬)
+    sessions = Study_TimerSession.objects.filter(user=request.user).order_by('-date')
+
+    # 기록을 timedelta 형식으로 변환
+    for session in sessions:
+        session.records = convert_to_timedelta(session.records)
+
+    # 오늘의 기록 가져오기 (가장 높은 기록)
+    today_sessions = sessions.filter(date__date=today)
+    today_record_value = None
+    today_record = None  # today_record 변수를 미리 정의
+    if today_sessions:
+        today_record = max(today_sessions, key=lambda session: session.records)
+        today_record_value = convert_to_timedelta(today_record.records)  # timedelta로 변환
+
+    # 친구들의 오늘의 공부 기록 가져오기
+    friends_records = []
+    for friendship in friends:
+        friend = friendship.friend
+        friend_today_sessions = Study_TimerSession.objects.filter(user=friend, date__range=(start_of_day, end_of_day))
+
+        if friend_today_sessions:
+            best_record = max(friend_today_sessions, key=lambda session: session.records)
+            friends_records.append((friend.username, convert_to_timedelta(best_record.records)))
+
+    # 나의 기록을 friends_records에 추가
+    if today_record_value:
+        friends_records.append((user.username, today_record_value))
+
+    # 기록을 기준으로 내림차순 정렬
+    friends_records.sort(key=lambda x: x[1], reverse=True)
+
+    context = {
+        'chapter': chapter,
+        'lectures': lectures,
+        'chapter_name': chapter_name,
+        'uploaded_files': uploaded_files,  # 업로드된 파일들을 context에 추가
+        'form': form,  # 폼을 context에 추가
+        'request_user': user,
+        'friend_requests': friend_requests,
+        'friends_records': friends_records,
+        'friends': friends,
+        'today_record': today_record
+        }
+
+    return render(request, "summary/AI_QUIZ.html", context)
+
+
 # 시간 문자열 시간으로 변환
 def convert_to_timedelta(record):
     # 시간 문자열을 ':'로 분할하여 시, 분, 초를 추출
@@ -771,37 +864,6 @@ def get_file_size(request, file_id):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import re
-# from django.shortcuts import render, get_object_or_404
-# from .models import UploadFile_summary
-
-
-
-
-
-
-
-
-
-
 def extract_quiz(input_text):
     # 패턴 수정: 문제, 보기, 답, 해설을 포함한 정규 표현식
     pattern = r'\[문제:([^\]]+)\]\s*1\.\s*([^\n]+)\s*2\.\s*([^\n]+)\s*3\.\s*([^\n]+)\s*4\.\s*([^\n]+)\s*\[답: (\d)\.\s*([^\]]+)\]'
@@ -890,7 +952,7 @@ def show_quiz_view(request, lecture_name, chapter_name, file_id):
         'lectures': lectures,
         'quiz_list': quiz_list
     }
-    return render(request, 'summary/AI_quizpage.html', context)
+    return render(request, 'summary/AI_QUIZ.html', context)
 
 
 
